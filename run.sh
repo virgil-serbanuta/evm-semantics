73593dc3688b2abaa97486724e463023f45b455a
Daejun Park <daejunpark@gmail.com>
2018-12-05 16:03:58 -0600

    transfer

--- run.sh
+++ run.sh
@@ -20,9 +20,13 @@
 # time kprove ~/verified-smart-contracts/specs/vyper-erc20/balanceOf-spec.k -v -d .build/java -m VERIFICATION --z3-executable --smt_prelude evm.smt2
 
 # time kprove orig-balanceOf-spec.k -v -d .build/java --z3-executable --smt_prelude evm.smt2
+# time kprove orig-transfer-success-1-spec.k -v -d .build/java --z3-executable --smt_prelude evm.smt2
 
 # time kompile -v --debug --backend java -d common common.k
 
 # time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 balanceOf-spec.k -s2 balanceOf-spec.k -sm1 BALANCEOF-SPEC -sm2 BALANCEOF-SPEC --smt_prelude evm.smt2 --z3-executable
 # time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 balanceOf1-spec.k -s2 balanceOf2-spec.k -sm1 BALANCEOF1-SPEC -sm2 BALANCEOF2-SPEC --smt_prelude evm.smt2 --z3-executable
-  time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 balanceOf1-spec.k -s2 balanceOf3-spec.k -sm1 BALANCEOF1-SPEC -sm2 BALANCEOF3-SPEC --smt_prelude evm.smt2 --z3-executable
+# time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 balanceOf1-spec.k -s2 balanceOf3-spec.k -sm1 BALANCEOF1-SPEC -sm2 BALANCEOF3-SPEC --smt_prelude evm.smt2 --z3-executable
+
+# time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 transfer-spec.k  -s2 transfer-spec.k  -sm1 TRANSFER-SPEC  -sm2 TRANSFER-SPEC  --smt_prelude evm.smt2 --z3-executable
+# time keq -v -d common -d1 .build/java -d2 .build/java -m1 ETHEREUM-SIMULATION -m2 ETHEREUM-SIMULATION -s1 transfer1-spec.k -s2 transfer2-spec.k -sm1 TRANSFER1-SPEC -sm2 TRANSFER2-SPEC --smt_prelude evm.smt2 --z3-executable
